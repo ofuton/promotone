b76c777c10bb7176c45aaa071e1f926d22f2e119
utakeru <takeru.u.cb@gmail.com>
2020-10-21 11:42:59 +0900

    initial commit

---
+++ src/app/content.ts
@@ -0,0 +1,19 @@
+import "../manifest.json"
+import "../styles/content.scss"
+import { html, render } from "lit-html"
+import DomObserver, { EventType } from "./kintone/dom-observer"
+
+const sayHello = (name: String) => {
+  return html`<h1>Hello, ${name}!!</h1>`
+}
+
+render(sayHello('ofuton'), document.body);
+
+const domObserver = new DomObserver()
+domObserver.startCommentComponentObserver()
+
+document.addEventListener(EventType.COMMENT_COMPONENT_LOADED, (e) => {
+  const targetEl = (e as CustomEvent).detail.element
+  console.log("comment component loaded", targetEl)
+})
+
